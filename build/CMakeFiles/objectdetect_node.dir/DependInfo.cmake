4914025139a319dae3fe627fa9e409f81167d50a
marhes-lab <36937615+shakeebbb@users.noreply.github.com>
2018-12-27 13:36:31 -0700

    IROS Competition Code

---
+++ build/CMakeFiles/objectdetect_node.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/objectdetect_ws/src/objectdetect_pkg/src/objectdetect_node.cpp" "/home/ubuntu/objectdetect_ws/src/objectdetect_pkg/build/CMakeFiles/objectdetect_node.dir/src/objectdetect_node.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_PACKAGE_NAME=\"objectdetect_pkg\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/ros/indigo/include"
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/objectdetect_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/objectdetect_node.dir/src/objectdetect_node.cpp.o"
+  "devel/lib/objectdetect_pkg/objectdetect_node.pdb"
+  "devel/lib/objectdetect_pkg/objectdetect_node"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/objectdetect_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
